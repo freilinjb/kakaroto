fdcbfba82bc698f44d15732273b4106f492474f0
freilinjb <freilinjb@gmail.com>
2020-12-16 23:02:20 -0400

    operaciones y estandares

--- index.php
+++ index.php
@@ -5,11 +5,15 @@ require_once "Controllers/UserController.php";
 require_once "Controllers/employeeController.php";
 require_once "Controllers/PruebaCrack.php";
 require_once "Controllers/ProductoController.php";
+require_once "Controllers/EstandarController.php";
+require_once "Controllers/OperacionesController.php";
 
 require_once "Models/EmployeeModel.php";
 require_once "Models/UserModel.php";
 require_once "Models/PruebaModel.php";
 require_once "Models/ProductoModel.php";
+require_once "Models/EstandarModel.php";
+require_once "Models/OperacionesModel.php";
 
 
 $template = new TemplateController();
--- views/partials/asside.php
+++ views/partials/asside.php
@@ -8,28 +8,12 @@
 
             <!-- Sidebar -->
             <div class="sidebar">
-                <!-- Sidebar user (optional) -->
-                
-
-                <!-- SidebarSearch Form -->
-                <!-- <div class="form-inline">
-                    <div class="input-group" data-widget="sidebar-search">
-                        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-sidebar">
-                                <i class="fas fa-search fa-fw"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div> -->
-
-                <!-- Sidebar Menu -->
                 <nav class="mt-2">
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                         <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                         <li class="nav-item">
-                            <a href="pages/gallery.html" class="nav-link">
+                            <a href="home" class="nav-link">
                                 <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>
                                     Dashboard
@@ -67,20 +51,6 @@
                                     <i class="fas fa-angle-left right"></i>
                                 </p>
                             </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="../UI/general.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>General</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="../UI/icons.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Icons</p>
-                                    </a>
-                                </li>
-                            </ul>
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link">
@@ -115,21 +85,9 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="administracionProducto" class="nav-link">
+                                    <a href="administracionProductos" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Administración de producto</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="../../index2.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Dashboard v2</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="../../index3.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Dashboard v3</p>
                                     </a>
                                 </li>
                             </ul>
@@ -143,13 +101,13 @@
                                 </p>
                             </a>
                             <ul class="nav nav-treeview">
-                                <li class="nav-item">
+                                <li class="plantas">
                                     <a href="../forms/general.html" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Asignar Planta</p>
                                     </a>
                                 </li>
-                                <li class="nav-item">
+                                <li class="modulos">
                                     <a href="../forms/advanced.html" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Asignar Modulo</p>
@@ -182,33 +140,13 @@
                         </li>
                         <li class="nav-header">CONTROLES DE ACCESO</li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="administracionUsuarios" class="nav-link">
                                 <i class="nav-icon fas fa-table"></i>
                                 <p>
-                                    Tables
+                                    Asignacion de usuarios
                                     <i class="fas fa-angle-left right"></i>
                                 </p>
                             </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="../tables/simple.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Simple Tables</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="../tables/data.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>DataTables</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="../tables/jsgrid.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>jsGrid</p>
-                                    </a>
-                                </li>
-                            </ul>
                         </li>
                     </ul>
                 </nav>
